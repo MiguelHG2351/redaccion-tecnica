2be14dad48e7b61d5c1fcb828f897be4cc42d06b
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-10-03 08:29:27 -0600

    route home

--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,3 @@ export default function Home({ posts }) {
     </ul>
 </>
 }
-
-export async function getStaticProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
-    const res = await fetch('https://redaccion-tecnica.vercel.app/api/user')
-    const posts = await res.json()
-  
-    // By returning { props: posts }, the Blog component
-    // will receive `posts` as a prop at build time
-    return {
-      props: {
-        posts,
-      },
-    }
-  }
