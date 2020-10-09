b255d14a2bfb4914decdcb36961696a08f061982
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-10-09 10:54:31 -0600

    testing os

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,5 +1,9 @@
+const os = require('os')
+
+
 export default (req, res) => {
     res.statusCode = 200
+    const platform = os.platform()
     res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ name: 'John Doe' }))
-  }
+    res.end(JSON.stringify({ name: 'John DoeS', platform }))
+}
