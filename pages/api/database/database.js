f9b57ccee233ffcafb320e8b1fcfc58241d3209d
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-11-10 04:30:29 -0600

    eslint add | config document | dark mode | MediaPlayer lvl 0

--- pages/api/database/database.js
+++ pages/api/database/database.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose'
 const DATABASE = process.env.DATABASE
 
 mongoose.connect(DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
 
-.then(() => console.log("Se conecto a la base de datos"))
-.catch(e => console.log(`F ${e}`))
+  .then(() => console.log('Se conecto a la base de datos'))
+  .catch(e => console.log(`F ${e}`))
