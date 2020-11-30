a7c9663cc2109f7838802377d672e73652211a73
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-11-30 13:09:11 -0600

    Modal Component

--- components/presentacion.js
+++ components/presentacion.js
@@ -1,18 +1,115 @@
-import styles from 'components/styles/presentacion'
+import { useReducer, useEffect } from "react";
+
+import styles, { cardStyle } from "components/styles/presentacion";
+
+const Modal = ({ name, image, description, description2, isVisible, dispatch }) => {
+    return (
+        <>
+            { isVisible &&
+            <>
+            <div className="modal">
+                <div className="center">
+                    <div className="image-user">
+                        <img src={image} alt={name}/>
+                    </div>
+                    <div className="info-user">
+                        <h2>{name}</h2>
+                        <p>{ description }</p>
+                        <p>{ description2 }</p>
+                    </div>
+                </div>
+            </div>
+            <div className="overlay" onClick={() => dispatch({type: 'hidden'})}></div>
+            </>
+            }
+            <style jsx>
+                { cardStyle }
+            </style>
+        </>
+    )
+};
 
 export default function Presentacion() {
+    function reducer(state, action) {
+        switch (action.type) {
+            case "Enrique":
+                return {
+                    name: "Rene Enrique Urbina Rivera",
+                    image: 'https://outlook.office.com/owa/service.svc/s/GetPersonaPhoto?email=rene.urbina95u%40std.uni.edu.ni&UA=0&size=HR96x96',
+                    description: 'Programador en Bajo Nivel con C/C++ ',
+                    description2: 'Modelado 3D',
+                    isVisible: true
+                };
+            case "Andre":
+                return {
+                    name: "Andre",
+                    image: '/images/XD.png',
+                    description: 'Programador con Python',
+                    description2: 'C/++',
+                    isVisible: true
+                };
+            case "Erick":
+                return {
+                    name: "Erick",
+                    image: '/images/XD.png',
+                    description: 'Universidad Nacional de Ingenieria',
+                    description2: 'C/++',
+                    isVisible: true
+                };
+            case "hidden":
+                return {
+                    name: "",
+                    image: '',
+                    description: '',
+                    description2: '',
+                    isVisible: false
+                };
+            default:
+                break;
+        }
+    }
+
+    const initialState = {
+        name: '',
+        image: '',
+        description: '',
+        description2: '',
+        isVisible: false
+
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        console.log(state)
+    }, [state])
+    
+
+    function Enrique() {
+        dispatch({type: 'Enrique'})
+    }
+
+    function Erick() {
+        dispatch({type: 'Erick'})
+    }
+
+    function Andre() {
+        dispatch({type: 'Andre'})
+    }
+
+
     return (
         <section className="presentacion">
+            <Modal {...state} dispatch={dispatch} />
+
             <h2>Proyecto Final de Redacción</h2>
-            
+
             <ul className="list-students">
-                <li>Enrique</li>
-                <li>Andre</li>
-                <li>Erick</li>
+                <li onClick={Enrique}>Enrique</li>
+                <li onClick={Andre}>Andre</li>
+                <li onClick={Erick}>Erick</li>
             </ul>
-            <style jsx>
-                { styles }
-            </style>
+            <style jsx>{styles}</style>
         </section>
     );
 }
--- components/styles/presentacion.js
+++ components/styles/presentacion.js
@@ -1,5 +1,70 @@
 import css from "styled-jsx/css";
 
+export const cardStyle = css`
+    .modal {
+        --size: 260px;
+        position: fixed;
+        left: calc(50% - (var(--size)) / 2);
+        top: calc(50% - (var(--size)) / 2);
+        /* background-image: linear-gradient(red, blue);
+         */
+        background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
+        width: var(--size);
+        height: var(--size);
+        z-index: 20;
+        padding: 18px;
+        box-sizing: border-box;
+    }
+
+    .info-user {
+        margin-top: 22px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: #fff;
+    }
+
+    .center {
+        margin: auto;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .info-user h2 {
+        margin-bottom: 18px;
+    }
+
+    .image-user {
+        display: flex;
+        justify-content: center;
+    }
+
+    .image-user img {
+        display: flex;
+        justify-content: center;
+        width: 30%;
+    }
+
+    @media screen and (min-width: 1200px) {
+        .modal {
+            --size: 450px;
+
+        }
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, 0.45);
+        z-index: 10;
+    }
+`
+
 export default css`
 
     .presentacion {
