a6043538b23f2abe3cb114d312470e59d391287a
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2020-12-04 15:30:29 -0600

    error page

--- pages/[carrera]/[video].js
+++ pages/[carrera]/[video].js
@@ -53,86 +53,30 @@ function GrupoVideo({router}) {
                             <h3>Regresar</h3>
                         </a>
                     </Link>
-                    <div className="header-group">
-                        <h1>Grupo 1</h1>
-                    </div>
-                    <ul className="list-group">
-                        <div className="line"></div>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-                            <Link href="/grupos/first">
-                                <a>Grupo 1</a>
-                            </Link>
-                        </li>
+                    {
+                        <>
+                        <div className="header-group">
+                            <h1>Grupo 1</h1>
+                        </div>
+                        <ul className="list-group">
+                            <div className="line"></div>
+                            <li className="group">
+                                <i className="material-icons">check_circle</i>
+                                <Link href="/grupos/first">
+                                    <a>Grupo 1</a>
+                                </Link>
+                            </li>
 
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
+                            <li className="group">
+                                <i className="material-icons">check_circle</i>
 
-                            <Link href="/group/2">
-                                <a>Recursos</a>
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className="header-group">
-                        <h1>Grupo 2</h1>
-                    </div>
-                    <ul className="list-group">
-                        <div className="line"></div>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/3">
-                                <a>Grupo 2</a>
-                            </Link>
-                        </li>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/4">
-                                <a>Recursos</a>
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className="header-group">
-                        <h1>Grupo 3</h1>
-                    </div>
-                    <ul className="list-group">
-                        <div className="line"></div>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/3">
-                                <a>Grupo 3</a>
-                            </Link>
-                        </li>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/3">
-                                <a>Recursos</a>
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className="header-group">
-                        <h1>Grupo 4</h1>
-                    </div>
-                    <ul className="list-group">
-                        <div className="line"></div>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/3">
-                                <a>Grupo 4</a>
-                            </Link>
-                        </li>
-                        <li className="group">
-                            <i className="material-icons">check_circle</i>
-
-                            <Link href="/group/3">
-                                <a>Recursos</a>
-                            </Link>
-                        </li>
-                    </ul>
+                                <Link href="/group/2">
+                                    <a>Recursos</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </>
+                    }
                 </section>
             </div>
             <style jsx>
